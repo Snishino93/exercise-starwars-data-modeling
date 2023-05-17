b3909f90ef2c90c5537f2d124ccefa3e515ee163
Shiki Nishino <105764723+Snishino93@users.noreply.github.com>
2023-05-17 20:20:55 +0000

    a media

--- src/models.py
+++ src/models.py
@@ -7,23 +7,102 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favourite = relationship('Favourite', backref='user', lazy=True)
+
+class Favourite(Base):
+    __tablename__ = 'favourite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'),
+        nullable=False)
+    starships = relationship('Starships', backref='user', lazy=True)
+    planets = relationship('Planets', backref='user', lazy=True)
+    character = relationship('Character', backref='user', lazy=True)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    diameter = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)
+    residents = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+    planets_id = Column(Integer, ForeignKey('planets.id'),
+        nullable=False)    
+    starships_id = Column(Integer, ForeignKey('starships.id'),
+        nullable=False)
+    favourite_id = Column(Integer, ForeignKey('favourite.id'),
+        nullable=True)
+
+class Starships(Base):
+    __tablename__ = 'starships'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+    model = Column(String(250), nullable=False)
+    starship_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    cost_in_credits = Column(String(250), nullable=False)
+    length = Column(String(250), nullable=False)
+    crew = Column(String(250), nullable=False)
+    passengers = Column(String(250), nullable=False)
+    max_atmosphering_speed = Column(String(250), nullable=False)
+    hyperdrive_rating = Column(String(250), nullable=False)
+    mglt = Column(String(250), nullable=False)
+    cargo_capacity = Column(String(250), nullable=False)
+    consumables = Column(String(250), nullable=False)
+    films = Column(String(250), nullable=False)
+    pilots = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+    planet = relationship("Planets", backref="character", uselist=False)
+    favourite_id = Column(Integer, ForeignKey('favourite.id'),
+        nullable=True)
+    
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False, unique=True)
+    birth_year = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    planet = relationship("Planets", backref="character", lazy=True)
+    films = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    starships = relationship("Starships", backref="character", uselist=False)
+    starship_id = Column(Integer, ForeignKey("starships.id"), nullable=False)
+    vehicles = Column(String(250), nullable=False)
+    url = Column(String(250), nullable=False)
+    created = Column(String(250), nullable=False)
+    edited = Column(String(250), nullable=False)
+    favourite_id = Column(Integer, ForeignKey('favourite.id'),
+        nullable=True)
 
     def to_dict(self):
         return {}
